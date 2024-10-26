df6ac8cc03eb66a99280b7dcaf2d81849b12ff7c
unknown <870148776.mty@gmail.com>
2024-10-25 20:45:37 -0600

    programa si

---
+++ EnvioGithub/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String box = sc.nextLine();
+    if(box!="Si"){
+        System.out.println("Hola Mundo");
+    }
+    else if(box!="No"){
+        System.out.println(" ");
+    }
+        }
+    }
+
